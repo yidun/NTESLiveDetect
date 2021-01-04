19c6eabc366d8f191d8b04e6c1bb7a9cc6cb0820
luolihao123456 <luolihao123456@163.com>
2021-01-04 14:31:43 +0800

    commit v2.2.4.beta

--- Classes/NTESBaseComponent.framework/Headers/NTESBaseComponent.h
+++ Classes/NTESBaseComponent.framework/Headers/NTESBaseComponent.h
@@ -2,7 +2,7 @@
 //  NTESBaseComponent.h
 //  NTESBaseComponent
 //
-//  Created by 罗礼豪 on 2020/9/10.
+//  Created by 罗礼豪 on 2020/10/16.
 //  Copyright © 2020 罗礼豪. All rights reserved.
 //
 
@@ -14,11 +14,8 @@ FOUNDATION_EXPORT double NTESBaseComponentVersionNumber;
 //! Project version string for NTESBaseComponent.
 FOUNDATION_EXPORT const unsigned char NTESBaseComponentVersionString[];
 
-#import "NTESBaseCrashManager.h"
-#import "NTESBaseHttpRequest.h"
-#import "NTESBaseWebViewController.h"
-
 // In this header, you should import all the public headers of your framework using statements like #import <NTESBaseComponent/PublicHeader.h>
 
-
-
+#import "NTESBaseCrashManager.h"
+#import "NTESBaseWebViewController.h"
+#import "NTESBaseHttpRequest.h"
--- Classes/NTESBaseComponent.framework/Headers/NTESBaseCrashManager.h
+++ Classes/NTESBaseComponent.framework/Headers/NTESBaseCrashManager.h
@@ -34,7 +34,6 @@ NS_ASSUME_NONNULL_BEGIN
 取消收集崩溃日志
 */
 - (void)cancelCollectCrash;
-
 @end
 
 NS_ASSUME_NONNULL_END
